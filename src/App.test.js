bd0176ef294ce56223a4ccb18f7dec70d01ccffc
Serge <serge.lal95@gmail.com>
2024-01-31 16:40:42 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,23 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// docker run -f Dockerfile.dev
+// docker build . 
+// docker build -t geers/test 
+// docker-compose up --build
+// docker-compose down 
+// docker run -it geers/test sh
+// docker exec geers/test sh 
+// docker ps 
+// docker kill 
+// docker stop 
+// docker run -p 3000/3000 
+// docker system prune : supprimer
+// docker attach :id : pour attacher le conteneur au terminal
